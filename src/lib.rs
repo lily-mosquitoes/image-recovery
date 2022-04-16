235021a8a826026c431a95259759f1a569a83dcd
Lílian <lily.mosquitoes@gmail.com>
2022-04-15 23:09:36 -0300

    update module imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#![feature(test)]
+extern crate test;
+
+pub mod ops;
+#[cfg(test)] mod ops_tests;
+pub mod img;
+#[cfg(test)] mod img_tests;
+pub mod utils;
+#[cfg(test)] mod utils_tests;
+
+pub mod solvers;
+#[cfg(test)] mod solvers_tests;
