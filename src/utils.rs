49fa8c249e7a101d96f394b458b8a0e8428ea99f
Lílian <lily.mosquitoes@gmail.com>
2022-04-15 23:07:17 -0300

    add utility functions for matrices

---
+++ src/utils.rs
@@ -0,0 +1,19 @@
+//! Utility functions for matrices (ndarray::Array2<f64>)
+
+use ndarray::Array2;
+use crate::ops::{Power};
+
+// length of vectors given two matrices, unchecked for size
+pub fn len_of_vectors(a: &Array2<f64>, b: &Array2<f64>) -> Array2<f64> {
+    (a.squared() + b.squared()).map(|x| x.sqrt())
+}
+
+// let len_of_gradient= |a, b| { len_of_vectors(a.dx(), a.dy()) };
+
+// the projection of vectors from two matrices into a 2D ball (-1, 1),
+// unchecked for size
+pub fn projection_onto_2d_ball(a: &Array2<f64>, b: &Array2<f64>) -> (Array2<f64>, Array2<f64>) {
+    let max = len_of_vectors(a, b).map(|x| 1_f64.max(*x));
+
+    (a / &max, b / &max)
+}
---
+++ src/utils_tests.rs
@@ -0,0 +1,58 @@
+use pretty_assertions::assert_eq;
+use test::{Bencher, black_box};
+use ndarray::{Array2, arr2};
+use crate::utils;
+
+static D_32: (usize, usize) = (32, 32);
+static D_1024: (usize, usize) = (1024, 1024);
+
+fn get_random_matrix(dimensions: (usize, usize)) -> Array2<f64> {
+    let mut matrix = Array2::<f64>::zeros(dimensions);
+    for x in 0..matrix.ncols() {
+        for y in 0..matrix.nrows() {
+            matrix[[x, y]] = rand::random::<u8>()  as f64;
+        }
+    }
+
+    matrix
+}
+
+#[test]
+fn len_of_vectors_is_correct() {
+    let a = &get_random_matrix(D_32);
+    let b = &get_random_matrix(D_32);
+
+    let test = utils::len_of_vectors(a, b);
+    let manual = ((a * a) + (b * b)).map(|x| x.sqrt());
+
+    assert_eq!(test, manual);
+}
+
+#[test]
+fn projection_onto_2d_ball_is_correct() {
+    let a = &arr2(&[[3.0, -0.5], [-3.0, -0.5]]);
+    let b = &arr2(&[[4.0, 0.5], [0.0, 0.5]]);
+
+    let proj_a = arr2(&[[0.6, -0.5], [-1.0, -0.5]]);
+    let proj_b = arr2(&[[0.8, 0.5], [0.0, 0.5]]);
+
+    let test_proj = utils::projection_onto_2d_ball(a, b);
+
+    assert_eq!(test_proj, (proj_a, proj_b));
+}
+
+#[bench]
+fn bench_len_of_vectors(bench: &mut Bencher) {
+    let a = &get_random_matrix(D_1024);
+    let b = &get_random_matrix(D_1024);
+
+    bench.iter(|| black_box(utils::len_of_vectors(a, b)));
+}
+
+#[bench]
+fn bench_projection_onto_2d_ball(bench: &mut Bencher) {
+    let a = &get_random_matrix(D_1024);
+    let b = &get_random_matrix(D_1024);
+
+    bench.iter(|| black_box(utils::projection_onto_2d_ball(a, b)));
+}
