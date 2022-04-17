a07bf56fc321b98b7098e9406c05b855e72c3e02
Lílian <lily.mosquitoes@gmail.com>
2022-04-17 04:19:06 -0300

    improve documentation for array_ops

--- src/array_ops.rs
+++ src/array_ops.rs
@@ -13,24 +13,26 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//! Traits for operations on matrices (`ndarray::Array2<f64>`)
+//! Traits for operations on matrices (`ndarray::Array2<f64>`).
 
+/// trait functions for differentiation
 pub trait Derivative {
-    // Derivative on the X axis (wrapping)
+    /// Derivative on the X axis (wrapping)
     fn dx(&self) -> Self;
-    // Derivative on the X axis, transposed (wrapping)
+    /// Derivative on the X axis, transposed (wrapping)
     fn dx_transposed(&self) -> Self;
-    // Derivative on the Y axis (wrapping)
+    /// Derivative on the Y axis (wrapping)
     fn dy(&self) -> Self;
-    // Derivative on the Y axis, transposed (wrapping)
+    /// Derivative on the Y axis, transposed (wrapping)
     fn dy_transposed(&self) -> Self;
 }
 
+/// trait functions for exponentiation
 pub trait Power {
-    // power of 2
+    /// power of 2
     fn squared(&self) -> Self;
-    // power of n, where n is an unsigned 32-bit integer
+    /// power of n, where n is an unsigned 32-bit integer
     fn powi(&self, n: i32) -> Self;
-    // power of n, where n is a 64-bit floating point number
+    /// power of n, where n is a 64-bit floating point number
     fn powf(&self, n: f64) -> Self;
 }
