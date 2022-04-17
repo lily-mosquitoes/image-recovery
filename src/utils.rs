95b550b126a9c5ba1b244ad5f76c1d6aa701a706
Lílian <lily.mosquitoes@gmail.com>
2022-04-17 04:14:15 -0300

    improve documentation for utils

--- src/utils.rs
+++ src/utils.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//! Utility functions for matrices (`ndarray::Array2<f64>`)
+//! Utility functions for matrices (`ndarray::Array2<f64>`).
 
 use ndarray::Array2;
 use crate::{
@@ -21,20 +21,21 @@ use crate::{
     array_ops::{Power},
 };
 
-// length of vectors given two matrices, unchecked for size
+/// length of vectors given two matrices, unchecked for size
 pub fn len_of_vectors(a: &Array2<f64>, b: &Array2<f64>) -> Array2<f64> {
     (a.squared() + b.squared())
         .map(|x| x.sqrt())
 }
 
-// length of vectors given two RGB matrices, unchecked for size
+/// length of vectors given two RGB matrices, unchecked for size
 pub fn len_of_vectors_multichannel(a: &RgbMatrices, b: &RgbMatrices) -> Array2<f64> {
     let l = a.squared() + b.squared();
 
     (l.red + l.green + l.blue).map(|x| x.sqrt())
 }
 
-// this encourages sharp edges
+// previously used this implementation, which incidentally
+// this encourages sharp edges for the output!
 // pub fn len_of_vectors_multichannel(a: &RgbMatrices, b: &RgbMatrices) -> Array2<f64> {
 //     let l = (a.squared() + b.squared())
 //         .map(|x| x.sqrt());
@@ -42,8 +43,7 @@ pub fn len_of_vectors_multichannel(a: &RgbMatrices, b: &RgbMatrices) -> Array2<f
 //     l.red + l.green + l.blue
 // }
 
-// the projection of vectors from two matrices into a 2D ball (-1, 1),
-// unchecked for size
+/// the projection of vectors from two matrices into a 2D ball (-1, 1), unchecked for size
 pub fn ball_projection(a: &Array2<f64>, b: &Array2<f64>) -> (Array2<f64>, Array2<f64>) {
     let max = len_of_vectors(a, b)
         .map(|x| 1_f64.max(*x));
@@ -51,8 +51,7 @@ pub fn ball_projection(a: &Array2<f64>, b: &Array2<f64>) -> (Array2<f64>, Array2
     (a / &max, b / &max)
 }
 
-// the projection of vectors from two RGB matrices into a 2D ball (-1, 1),
-// unchecked for size
+/// the projection of vectors from two RGB matrices into a 2D ball (-1, 1), unchecked for size
 pub fn ball_projection_multichannel(a: &RgbMatrices, b: &RgbMatrices) -> (RgbMatrices, RgbMatrices) {
     let max = len_of_vectors_multichannel(a, b)
         .map(|x| 1_f64.max(*x));
