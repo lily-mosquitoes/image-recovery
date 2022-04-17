a2c60802aab9cd4468b191c2e133ee75646ef9ae
Lílian <lily.mosquitoes@gmail.com>
2022-04-17 15:39:54 -0300

    improve documentations of len of vectors

--- src/utils.rs
+++ src/utils.rs
@@ -21,13 +21,15 @@ use crate::{
     array_ops::{Power},
 };
 
-/// length of vectors given two matrices, unchecked for size
+/// length of vectors given two matrices, unchecked for size.
 pub fn len_of_vectors(a: &Array2<f64>, b: &Array2<f64>) -> Array2<f64> {
     (a.squared() + b.squared())
         .map(|x| x.sqrt())
 }
 
-/// length of vectors given two RGB matrices, unchecked for size
+/// length of vectors given two RGB matrices, unchecked for size.
+///
+/// This modification is inspired by the work of Bredies, K. (2014).
 pub fn len_of_vectors_multichannel(a: &RgbMatrices, b: &RgbMatrices) -> Array2<f64> {
     let l = a.squared() + b.squared();
 
@@ -43,7 +45,7 @@ pub fn len_of_vectors_multichannel(a: &RgbMatrices, b: &RgbMatrices) -> Array2<f
 //     l.red + l.green + l.blue
 // }
 
-/// the projection of vectors from two matrices into a 2D ball (-1, 1), unchecked for size
+/// the projection of vectors from two matrices into a 2D ball (-1, 1), unchecked for size.
 pub fn ball_projection(a: &Array2<f64>, b: &Array2<f64>) -> (Array2<f64>, Array2<f64>) {
     let max = len_of_vectors(a, b)
         .map(|x| 1_f64.max(*x));
@@ -51,7 +53,7 @@ pub fn ball_projection(a: &Array2<f64>, b: &Array2<f64>) -> (Array2<f64>, Array2
     (a / &max, b / &max)
 }
 
-/// the projection of vectors from two RGB matrices into a 2D ball (-1, 1), unchecked for size
+/// the projection of vectors from two RGB matrices into a 2D ball (-1, 1), unchecked for size.
 pub fn ball_projection_multichannel(a: &RgbMatrices, b: &RgbMatrices) -> (RgbMatrices, RgbMatrices) {
     let max = len_of_vectors_multichannel(a, b)
         .map(|x| 1_f64.max(*x));
